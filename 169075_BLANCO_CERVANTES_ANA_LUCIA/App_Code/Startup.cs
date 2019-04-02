5b88622e37707409daca71005a8fd9f6734c3d57
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-04-02 09:28:15 -0700

    Agregar archivos de proyecto.

---
+++ 169075_BLANCO_CERVANTES_ANA_LUCIA/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_169075_BLANCO_CERVANTES_ANA_LUCIA.Startup))]
+namespace _169075_BLANCO_CERVANTES_ANA_LUCIA
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
